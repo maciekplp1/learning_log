c1944892eab8519c19109edc71a34b41ef980f64
maciekplp <maciekplp@gmail.com>
2021-06-11 18:36:24 +0200

    debug off

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -141,6 +141,6 @@ import django_heroku
 django_heroku.settings(locals())
 
 if os.environ.get('DEBUG') == 'TRUE':
-    DEBUG = False
+    DEBUG = True
 elif os.environ.get('DEBUG') == 'FALSE':
     DEBUG = False
